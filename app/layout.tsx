3c98f8c1c5ef7071f4b422f474b44fcef560490a
ryandcsg <ryand@dcsg.com.au>
2024-01-09 14:13:59 +1100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
